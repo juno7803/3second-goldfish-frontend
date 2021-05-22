107450773eac80233ba25a83a03ce375b41fdc55
pa-rang <appdler96@kaist.ac.kr>
2021-05-23 01:58:34 +0900

    feat(Result): add memory-image

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,28 +1,57 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import { convertHtmlToPng } from '../../lib/utils/convertHtmlToPng';
 
-function Home() {
-	const [img, setImg] = React.useState<any>(undefined);
+const Styled = {
+	ImageWrapper: styled.div`
+		box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.15);
+		border-radius: 8px;
+		width: 320px;
+		height: 320px;
+		background-color: #fff;
+		padding: 24px;
+		font-size: 32px;
+		color: #363636;
+		& > div {
+			margin-bottom: 16px;
+		}
+	`,
+};
 
-	const imageRef = React.useRef<HTMLElement>(null);
+const resultData = {
+	name: '이정연',
+	date: '5월 23일',
+};
+
+function Result() {
+	const [memoryImage, setMemoryImage] = React.useState<any>(undefined);
+	const memoryImageRef = React.useRef<HTMLElement>(null);
+
 	React.useEffect(() => {
 		(async () => {
-			const image = await convertHtmlToPng(imageRef);
-			console.log(`image`, image);
-			setImg(image);
+			const image = await convertHtmlToPng(memoryImageRef);
+			setMemoryImage(image);
 		})();
 	}, []);
 
+	const memoryHtml = (
+		<Styled.ImageWrapper ref={memoryImageRef as React.LegacyRef<HTMLDivElement> | undefined}>
+			<div>오늘 이정연은</div>
+			<div>웹파트 사람들을 만나</div>
+			<div>맛있는걸 먹으며</div>
+			<div>솝커톤을 했다.</div>
+		</Styled.ImageWrapper>
+	);
+
 	return (
 		<div>
-			<h1>이미지 테스트</h1>
-			<div style={{ border: '1px solid red' }} ref={imageRef as React.LegacyRef<HTMLDivElement> | undefined}>
-				<h1>TEST</h1>
-				<div>나를 이미지로 뽑아주세요</div>
-			</div>
-			<img src={img?.src} />
+			<h1>당신의 기억을 정리해드렸어요</h1>
+			{memoryHtml}
+			<a href={memoryImage?.src} download={`${resultData.name}-${resultData.date}의 기억.png`}>
+				이미지 다운로드
+			</a>
 		</div>
 	);
 }
 
-export default Home;
+export default Result;
