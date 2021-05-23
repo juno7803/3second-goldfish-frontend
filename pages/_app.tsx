f3c8902fe51e18d74eef222bedd859c6b7100f5f
pa-rang <appdler96@kaist.ac.kr>
2021-05-23 14:14:15 +0900

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
+			<Head>
+				<title>궁금붕어</title>
+				<meta name="description" content="궁금붕어" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<Component {...pageProps} />
 		</RecoilRoot>
 	);
